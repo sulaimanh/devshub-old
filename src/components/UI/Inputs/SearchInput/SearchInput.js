4e939dab69ac9a14d9cbaa843e803649fac1de50
Sulaiman Hamouda <hamouda.sulaiman@gmail.com>
2020-07-02 01:44:09 -0400

    toggle filter menu

--- src/components/UI/Inputs/SearchInput/SearchInput.js
+++ src/components/UI/Inputs/SearchInput/SearchInput.js
@@ -1,30 +1,73 @@
 import React, { useState } from "react";
 import styles from "./SearchInput.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSearch,
+  faCaretDown,
+  faCaretUp
+} from "@fortawesome/free-solid-svg-icons";
 
 const SearchInput = (props) => {
   const [difficulty, setDifficulty] = useState({
-    showDropDown: false,
+    toggleDropDown: false,
     difficulty: ""
   });
 
+  const toggleDropDown = () => {
+    setDifficulty({
+      ...difficulty,
+      toggleDropDown: !difficulty.toggleDropDown
+    });
+  };
+
   const difficultyHandler = (choice) => {
-    console.log(choice);
-    setDifficulty(choice);
+    if (choice === difficulty.difficulty) {
+      setDifficulty({ ...difficulty, difficulty: "" });
+    } else {
+      setDifficulty({ ...difficulty, difficulty: choice });
+    }
   };
 
   return (
     <div className={styles.input}>
-      <input placeholder="Search" className={styles.input__search} />
-      <div className={styles.input__dropdown}>
+      <div className={styles.input__input}>
+        <input placeholder="Search" className={styles.input__search} />
+        <a onClick={toggleDropDown} className={styles.input__caret}>
+          <FontAwesomeIcon
+            icon={difficulty.toggleDropDown ? faCaretUp : faCaretDown}
+            size="2x"
+          />
+        </a>
+
+        <FontAwesomeIcon
+          icon={faSearch}
+          size="lg"
+          className={styles.input__icon}
+        />
+        <button
+          onClick={props.handler}
+          className={styles.input__search__button}
+        >
+          Submit
+        </button>
+      </div>
+
+      <div
+        className={[
+          styles.input__dropdown,
+          difficulty.toggleDropDown ? null : styles.input__hideDropdown
+        ].join(" ")}
+      >
         <h2 className={styles.input__dropdown__text}>
           Select to sort by Difficulty
         </h2>
         <p
           className={[
             styles.input__dropdown__difficulty,
-            styles.input__dropdown__easy
+            styles.input__dropdown__easy,
+            difficulty.difficulty === "easy"
+              ? styles.input__dropdown__easy__chosen
+              : null
           ].join(" ")}
           onClick={() => difficultyHandler("easy")}
         >
@@ -33,7 +76,10 @@ const SearchInput = (props) => {
         <p
           className={[
             styles.input__dropdown__difficulty,
-            styles.input__dropdown__medium
+            styles.input__dropdown__medium,
+            difficulty.difficulty === "medium"
+              ? styles.input__dropdown__medium__chosen
+              : null
           ].join(" ")}
           onClick={() => difficultyHandler("medium")}
         >
@@ -42,21 +88,16 @@ const SearchInput = (props) => {
         <p
           className={[
             styles.input__dropdown__difficulty,
-            styles.input__dropdown__difficult
+            styles.input__dropdown__hard,
+            difficulty.difficulty === "hard"
+              ? styles.input__dropdown__hard__chosen
+              : null
           ].join(" ")}
           onClick={() => difficultyHandler("hard")}
         >
           Hard
         </p>
       </div>
-      <FontAwesomeIcon
-        icon={faSearch}
-        size="lg"
-        className={styles.input__icon}
-      />
-      <button onClick={props.handler} className={styles.input__search__button}>
-        Submit
-      </button>
     </div>
   );
 };
